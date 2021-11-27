d647da95a21551feb72405ac8666c3c01cd1d978
Víctor Andrés Rojas <victandres98@gmail.com>
2021-11-27 14:07:34 -0500

    Creating the Spotify login

---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+const Login = ({ providers }) => {
+  return (
+    <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
+      <img src="https://links.papareact.com/9xl" alt="" className="w-52 mb-5" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="bg-[#18D860] text-white p-5 rounded-lg"
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Login;
+
+export const getServerSideProps = async () => {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+};
